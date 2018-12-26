19d036e4be2a9db5939ed87fbeccc4805f0a2697
Karl Dreher <karldreher@outlook.com>
2018-12-26 16:24:38 -0700

    Update for new Netflix UI.

--- netflixplus.js
+++ netflixplus.js
@@ -1,23 +1,21 @@
 function DVDpage(){
 	// this function is for adding an "instant" button on individual DVD pages.  
 	
-	Button = document.getElementsByClassName("btn-play")
-	if (Button.length == 1){
-		url = Button[0].href
-		movieid = url.split("movieid=")[1].split("&")[0]
+	Button = document.getElementById("buttonContainer")
+		url = window.location.href
+		movieid = url.split("\/")[url.split("\/").length -1]
+		
 		var newurl = "https://netflix.com/title/" + movieid;
 		console.log(newurl);
 	
 	
 		var movielink = document.createElement("a"); 
-		movielink.setAttribute("align", "left"); 
 		movielink.setAttribute("href", newurl);
-		movielink.setAttribute("style", "float: left\; background: red\; width: 105%\; height: 15%\; padding: 2%\; marginTop: 40px\; border-radius: 5%\; font-size: 110%\; color: white\; text-align: center\;");
+		movielink.setAttribute("style", "position: relative\; top: 15px\; background: red\; padding: 2%\; height: 60px\; border-radius: 5%\; font-size: 130%\; color: white\; text-align: center\;");
 
 
 		movielink.textContent = "Instant";
-		Button[0].parentNode.parentNode.appendChild(movielink);
-	};
+		Button.appendChild(movielink);
 }
 
 DVDpage()
